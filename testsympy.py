c0d68efa9e184cae1e76081198e400522f4f959e
vkd225 <vkd225@nyu.edu>
2017-04-11 12:19:09 -0400

    adding functions to sympy

--- testsympy.py
+++ testsympy.py
@@ -1,28 +1,29 @@
 
 from sympy import *
+from sympy.parsing.sympy_parser import parse_expr
 from sympy.abc import * 
 
-# def user_input(exp):
-# 	exp = raw_input('Enter User Input: ')
-# 	print (exp)
-# 	return exp
+def user_input(exp):
+ 	exp = raw_input('Enter User Input: ')
+ 	exp = parse_expr(exp)
+ 	return exp
 
 
-# def step_input(exp):
-# 	exp = raw_input('Enter Step Output: ') 
-# 	return exp
+def step_input(exp):
+	exp = raw_input('Enter Step Output: ')
+	exp = parse_expr(exp)
+	return exp
 
-def check(exp_user, exp_step):
-	# user = user_input(exp_user)
-	# step = step_input(exp_step)
-	return (simplify (exp_user - exp_step) == 0)
+def check():
+	user = user_input(exp)
+	step = step_input(exp)
+	return (simplify (user - step) == 0)
 
 
-vers1 = sin(2*x)
-print type (vers1)
-vers2 = 2 *sin (x)*cos(x)
-print type (vers2)
-
-out = check(vers1,vers2)
+out = check()
 print out 
 
+
+# ver1 = sin(x)**2 + cos(x)**2
+# ver2 = 1
+# print simplify(ver1-ver2) == 0
